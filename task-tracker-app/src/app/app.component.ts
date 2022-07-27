f25d435dc07973f0b1b0412bf4fc34f8c57aa875
tatianaoxwell <oxwelltatiana@gmail.com>
2022-07-27 10:39:56 +0930

    header

---
+++ task-tracker-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'task-tracker-app';
+}
+openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
---
+++ task-tracker-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DialogComponent } from './dialog/dialog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+	MatToolbarModule,
+	MatIconModule,
+	BrowserAnimationsModule,
+	MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
