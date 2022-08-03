4f09f85a8281b445c7ffcc385aa1ca5674358209
tatianaoxwell <oxwelltatiana@gmail.com>
2022-08-03 16:37:03 +0930

    filter

--- task-tracker-app/src/app/app.component.ts
+++ task-tracker-app/src/app/app.component.ts
@@ -1,29 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IToDoItem } from './to-do-item.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Task Tracker App';
-  filterBy: string = '';
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+	return this._listFilter;
+  }
+  set listFilter(value: string) {
+	this._listFilter = value;
+	this.filteredList = this.performFilter(value);
+  }
+  filteredList: IToDoItem[] = [];
+  
   toDoList: IToDoItem[] = [
-	{
-		title: 'Go shopping',
-		isComplete: false,
-		dateCreated: new Date(),
-	},
-	{
-		title: 'Put shopping away',
-		isComplete: false,
-		dateCreated: new Date(),
-	},
-	{
-		title: 'Cook dinner',
-		isComplete: true,
-		dateCreated: new Date(),
-	}
-]
+    {
+      title: 'Go shopping',
+      isComplete: false,
+      dateCreated: new Date(),
+    },
+    {
+      title: 'Put shopping away',
+      isComplete: false,
+      dateCreated: new Date(),
+    },
+    {
+      title: 'Cook dinner',
+      isComplete: true,
+      dateCreated: new Date(),
+    },
+  ];
+  ngOnInit(): void {
+    this.listFilter = 'default';
+  }
+  performFilter(valueToFilterBy: string): IToDoItem[] {
+	valueToFilterBy = valueToFilterBy.toLowerCase();
+	return this.toDoList.filter((item: IToDoItem) =>
+		item.title.toLowerCase().includes(valueToFilterBy));
+  }
 }
