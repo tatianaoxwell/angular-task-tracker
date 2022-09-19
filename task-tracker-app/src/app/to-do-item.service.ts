086e39c0fb1cc7f21fa5e84ae0fab03ee7cff298
tatianaoxwell <oxwelltatiana@gmail.com>
2022-09-19 12:46:07 +0800

    setup of http.get

--- task-tracker-app/src/app/to-do-item.service.ts
+++ task-tracker-app/src/app/to-do-item.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { IToDoItem } from './to-do-item.model';
 
 @Injectable({
@@ -6,24 +9,35 @@ import { IToDoItem } from './to-do-item.model';
 })
 @Injectable()
 export class ToDoItemService {
-  getToDoList(): IToDoItem[] {
-    return [
-      {
-        title: 'Go shopping',
-        isComplete: false,
-        dateCreated: new Date(),
-      },
-      {
-        title: 'Put shopping away',
-        isComplete: false,
-        dateCreated: new Date(),
-      },
-      {
-        title: 'Cook dinner',
-        isComplete: false,
-        dateCreated: new Date(),
-      },
-    ];
+  
+  inputTodo: string = '';
+
+  constructor(private http: HttpClient) {}
+  private apiURL = 'http://localhost:3000/';
+
+
+  // Read/Get
+  getToDoList(): Observable<IToDoItem[]> {
+    return this.http.get<IToDoItem[]>(this.apiURL + 'todo?id=1').pipe(
+		catchError(this.handleError<IToDoItem[]>('getEvents', []))
+		);
   }
-  constructor() {}
+
+  private handleError<T> (operation = 'operation', result?: T) {
+	return (error: any): Observable<T> => {
+		console.log(error);
+		return of(result as T);
+	}
+  }
+
+  // Post
+  addTodoItem(inputTodo: IToDoItem): Observable<IToDoItem> {
+    return this.http.post<IToDoItem>(this.apiURL + 'todo?id=1', inputTodo);
+  }
+
+  // Delete
+  deleteItem(id: number): Observable<unknown> {
+    const url = `${this.apiURL}/${id}`;
+    return this.http.delete(url);
+  }
 }
